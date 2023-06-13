1c1751f94812371d7ea6d6ecfcf1ef4ba03dfe1c
Casper Zmuda <zmudakacper03@gmail.com>
2023-06-13 20:29:52 +0200

    Created starting sequence

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,44 @@
 'use strict';
 
-console.log('JS connected...');
+const playGameBtn = document.querySelector('.form__button');
+let restaurantName = document.querySelector('.form__input');
+const welcomeText = document.querySelector('.welcome-text');
+const welcomeTextText = document.querySelector('.welcome-text__text');
+let welcomeTextRestaurantName = document.querySelector(
+	'.welcome-text__restaurant'
+);
+
+const startGame = e => {
+	if (restaurantName.value) {
+		e.preventDefault();
+		restaurantName = restaurantName.value;
+		const startMenuContent = document.querySelector('.start-menu');
+		const startMenuFooter = document.querySelector('.footer');
+		startMenuContent.classList.add('disabled');
+		startMenuFooter.classList.add('disabled');
+		welcomeText.classList.remove('disabled');
+		welcomeTextRestaurantName.textContent = restaurantName;
+		welcomeTextInformation();
+		// loadGame();
+	}
+};
+
+const welcomeTextInformation = () => {
+	welcomeTextText.classList.add('animation1');
+	welcomeTextRestaurantName.classList.add('animation2');
+	setTimeout(() => {
+		welcomeTextText.classList.add('disabled');
+		welcomeTextRestaurantName.classList.add('disabled');
+	}, 8000);
+};
+
+playGameBtn.addEventListener('click', startGame);
+playGameBtn.addEventListener('keydown', e => {
+	if ((e.key = 'enter')) {
+		startGame();
+	}
+});
+
+// const loadGame = () => {
+  // document.title = '0 - Pizza Clicker 🍕';
+// };
